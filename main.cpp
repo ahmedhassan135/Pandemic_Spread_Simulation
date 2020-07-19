d03dc01b4a6fe38633047af10d0c462820b7a0ac
unknown <ahmedhassan.135@gmail.com>
2020-07-19 16:22:06 +0500

    added basic movement functionality methods to humans

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <conio.h>
 using namespace std;
 
 
@@ -22,6 +25,10 @@ class Human
     Coordinates current_location;
     bool infected;
     float probability_of_infection;
+
+
+    int GetHumanID() { return ID; }
+
 };
 
 class Working : public Human
@@ -32,7 +39,7 @@ class Working : public Human
     int travelling;
     int ticks_stuck;
 
-    void initialize_variables(int id, int Current_location_x, int Current_location_y, bool Infected, float Probability_of_infection, int House_address_x, int House_address_y, int Office_address_x, int Office_address_y)
+    void initialize_variables(int id, int Current_location_x, int Current_location_y, bool Infected, float Probability_of_infection, int Office_address_x, int Office_address_y)
     {
         ID = id;
         current_location.x = Current_location_x;
@@ -40,8 +47,8 @@ class Working : public Human
         infected = Infected;
         probability_of_infection = Probability_of_infection;
 
-        house_address.x = House_address_x;
-        house_address.y = House_address_y;
+        house_address.x = Current_location_x;       //Starting location is house location initially
+        house_address.y = Current_location_y;
         office_address.x = Office_address_x;
         office_address.y = Office_address_y;
         travelling = 0;                         //Travelling to office at start
@@ -50,38 +57,29 @@ class Working : public Human
 
     }
 
-    Coordinates GetCurrentLocation()
+    Coordinates GetCurrentLocation() { return current_location; }
+
+    Coordinates GetHouseAddress() { return house_address; }
+
+    Coordinates GetOfficeAddress() { return office_address; }
+
+    void SetCurrentLocation(int New_location_x, int New_location_y)
     {
-        return current_location;
+        current_location.x = New_location_x;
+        current_location.y = New_location_y;
     }
 
-    Coordinates GetHouseAddress()
+    void SetHouseAddress(int New_house_address_x, int New_house_address_y)
     {
-        return house_address;
-    }
-
-    Coordinates GetOfficeAddress()
-    {
-        return office_address;
-    }
-
-    void SetCurrentLocation(int Current_location_x, int Current_location_y)
-    {
-        current_location.x = Current_location_x;
-        current_location.y = Current_location_y;
-    }
-
-    void SetHouseAddress(int House_address_x, int House_address_y)
-    {
-        house_address.x = House_address_x;
-        house_address.y = House_address_y;
+        house_address.x = New_house_address_x;
+        house_address.y = New_house_address_y;
     }
 
 
-    void SetOfficeAddress(int Office_address_x, int Office_address_y)
+    void SetOfficeAddress(int New_Office_address_x, int New_Office_address_y)
     {
-        office_address.x = Office_address_x;
-        office_address.y = Office_address_y;
+        office_address.x = New_Office_address_x;
+        office_address.y = New_Office_address_y;
     }
 
 
@@ -100,10 +98,7 @@ class NotWorking : public Human
         probability_of_infection = Probability_of_infection;
     }
 
-    Coordinates GetCurrentLocation()
-    {
-        return current_location;
-    }
+    Coordinates GetCurrentLocation() { return current_location; }
 
 };
 
@@ -112,6 +107,7 @@ class NotWorking : public Human
 #define cols 6
 
 Cell world[rows][cols];
+Working h1[2];
 
 
 void initialize()
@@ -131,17 +127,26 @@ void initialize()
     //Place each on the world grid
 
 
-    //world[2][3].occupied_by_id = 34;
+    world[2][3].occupied_by_id = 34;    //Consider putting this in human initialization function
+    world[4][4].isOffice = true;        //Consider putting this in human initialization function
+
+
+    h1[0].initialize_variables(34, 3, 2, false, 0.2, 4, 4 );
+
 }
 
 
 void display_world()
 {
+
     for (int i = 0; i < rows; i++)
     {
         for(int j = 0; j < cols; j++)
         {
-            cout<<world[i][j].occupied_by_id<<"\t";
+            if (!world[i][j].isOffice)
+                cout<<world[i][j].occupied_by_id<<"\t";
+            else
+                cout<<"W"<<"\t";
         }
         cout<<endl<<endl;
     }
@@ -151,6 +156,75 @@ void display_world()
 //TODO: contact and probability of infection
 //TODO: movement of a human
 
+bool CheckIfNextBlockEmpty(int next_x, int next_y)      //returns false if next space is occupied else true if empty
+{
+    if (world[next_y][next_x].occupied_by_id == 0 && world[next_y][next_x].isOffice == false)
+        return true;
+    else
+        return false;
+}
+
+void MoveHuman(Working &h1, int next_x, int next_y)
+{
+    //empty previous cell in the world
+    world[h1.GetCurrentLocation().y][h1.GetCurrentLocation().x].occupied_by_id = 0;
+
+    //move human
+    h1.SetCurrentLocation(next_x, next_y);
+    world[next_y][next_x].occupied_by_id = h1.GetHumanID();
+
+
+
+}
+
+void MakeMove(Working &h1)
+{
+    //First move in the x axis
+    if (h1.GetCurrentLocation().x < h1.GetOfficeAddress().x)
+    {
+        //Check if next space empty
+        if (CheckIfNextBlockEmpty(h1.GetCurrentLocation().x + 1, h1.GetCurrentLocation().y))
+        {
+            //Move to next location
+            MoveHuman(h1, h1.GetCurrentLocation().x + 1, h1.GetCurrentLocation().y);
+        }
+    }
+    else if (h1.GetCurrentLocation().x > h1.GetOfficeAddress().x)
+    {
+        //Check if next space empty
+        if (CheckIfNextBlockEmpty(h1.GetCurrentLocation().x - 1, h1.GetCurrentLocation().y))
+        {
+            //Move to next location
+            MoveHuman(h1, h1.GetCurrentLocation().x - 1, h1.GetCurrentLocation().y);
+        }
+    }
+    else if (h1.GetCurrentLocation().x == h1.GetOfficeAddress().x)
+    {
+        if (h1.GetCurrentLocation().y < h1.GetOfficeAddress().y)
+        {
+            //Check if next space empty
+            if (CheckIfNextBlockEmpty(h1.GetCurrentLocation().x, h1.GetCurrentLocation().y + 1))
+            {
+                //Move to next location
+                MoveHuman(h1, h1.GetCurrentLocation().x, h1.GetCurrentLocation().y + 1);
+            }
+        }
+        else if (h1.GetCurrentLocation().y > h1.GetOfficeAddress().y)
+        {
+            //Check if next space empty
+            if (CheckIfNextBlockEmpty(h1.GetCurrentLocation().x, h1.GetCurrentLocation().y - 1))
+            {
+                //Move to next location
+                MoveHuman(h1, h1.GetCurrentLocation().x, h1.GetCurrentLocation().y -1 );
+            }
+        }
+    }
+
+
+
+    //Second move in the y axis when x = 0
+}
+
 //TODO: Display number of people currently infected and recovered.
 
 
@@ -158,7 +232,16 @@ void display_world()
 int main()
 {
     initialize();
-    display_world();
+
+    while(true)
+    {
+        system("cls");
+        display_world();
+        MakeMove(h1[0]);
+        //cout<<"\nLocation is "<< h1[0].GetCurrentLocation().x << endl <<endl;
+        getch();
+    }
+
 
 
 
